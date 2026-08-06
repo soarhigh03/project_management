642d6a9ce3533006acbe43ddecb482b751d9922e
Eunwoo Choi <soarhigh03@gmail.com>
2026-08-06 16:57:05 +0900

    feat: Remove commented pw

--- config/projects.js
+++ config/projects.js
@@ -16,7 +16,6 @@ export default [
   {
     id: 'demo',                     // URL에 쓰임: /p/demo/... (소문자·숫자·하이픈)
     name: '데모 프로젝트',
-    // 비밀번호: demo1234 (배포 전에 꼭 바꾸세요!)
     passwordHash: 'scrypt:16384:8:1:870878e0391a6b2cf8941491e6b264ad:4cc9f2b81bf31b20a30b1a354e7de349f2a67a9b92f4e0789b2c299e55fe65af',
     members: ['미잘', '은우', '팀원A'],
     regions: ['홈', '검색', '설정', '프로필', '기타'],
@@ -31,7 +30,6 @@ export default [
   {
     id: 'patz',
     name: 'patz',
-    // 비밀번호: threeever
     passwordHash: 'scrypt:16384:8:1:a3107cbc72e79a9de93c081bbbe019fc:ed89a334818401622794c80f389725b55cc5fd167375ced6c7e2d2cb3ca1d4f0',
     members: ['채은', '민우', '은우'],
     regions: ['홈', '탐색', '지도', '커뮤니티', '설정', 'DB'],
@@ -40,7 +38,6 @@ export default [
   {
     id: 'jarvis',
     name: 'jarvis',
-    // 비밀번호: stark
     passwordHash: 'scrypt:16384:8:1:7471c5ef3293f28b2523ee8c85f1a42a:01b24325d8e35cdfe05d51a3ad9bcc6772c2eea659b002aae96f27e287cd81d9',
     members: ['호승', '동휘', '유리', '은우'],
     regions: ['디자인', 'AI', '프론트', '기획'],
@@ -49,7 +46,6 @@ export default [
   {
     id: 'mijal',
     name: '말미잘',
-    // 비밀번호: mijal
     passwordHash: 'scrypt:16384:8:1:5d9051180d202be50753be732c0cd77e:7c8f14327c41930f7492f66c17dd1367eeecdf93b0efad03dee5fc0ac68dd51d',
     members: ['병호', '경민', '은우'],
     regions: ['홈', '매일 한마디', '세줄요약', '따라 읽기', '스트릭', '설정'],
